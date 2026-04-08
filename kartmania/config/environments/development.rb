c63dbbf25b677e6fe1f21b6dac2687b88593361a
a23ricardosr1 <a23ricardosr@iessanclemente.net>
2026-04-09 01:17:49 +0200

    Update of configuration and additions

--- kartmania/config/environments/development.rb
+++ kartmania/config/environments/development.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  # URL por defecto para los emails de Devise en desarrollo
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
