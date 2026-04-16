56e2bd60a56b0b693b4ebd7df1c23850a6d8fe51
jglgap <a23ricardosr@iessanclemente.net>
2026-04-16 13:45:18 +0200

    Update on landing pages

--- kartmania/app/javascript/application.js
+++ kartmania/app/javascript/application.js
@@ -2,3 +2,27 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+//import { animate, stagger } from "animejs"
+//
+//const initStaggerText = () => {
+//  // Selecciona TODOS los elementos con la clase
+//  const elements = document.querySelectorAll(".stagger-text");
+//  console.log("elementos encontrados:", elements.length)
+//
+//  if (!elements.length) return;
+//
+//  // Itera sobre cada uno
+//  elements.forEach(el => {
+//    el.innerHTML = el.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+//  });
+//
+//  animate(".stagger-text .letter", {
+//    opacity: [0, 1],
+//    translateY: [20, 0],
+//    delay: stagger(50),
+//    easing: "easeOutExpo",
+//  });
+//};
+//
+//document.addEventListener("turbo:load", initStaggerText);
+//document.addEventListener("DOMContentLoaded", initStaggerText);
