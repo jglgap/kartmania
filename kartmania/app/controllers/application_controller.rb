6e7594e611eab7a3de564a692367c8725d972da4
a23ricardosr1 <a23ricardosr@iessanclemente.net>
2026-05-04 18:37:59 +0200

    problemas de login resuelto

--- kartmania/app/controllers/application_controller.rb
+++ kartmania/app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
 
-    layout :layout_por_controlador
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-    private
+  layout :layout_por_controlador
 
-    def layout_por_controlador
-        devise_controller? ? "devise" : "application"
-    end
+  private
+
+  def layout_por_controlador
+    devise_controller? ? "devise" : "application"
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :direccion, :ciudad, :provincia, :codigo_postal, :telefono])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :direccion, :ciudad, :provincia, :codigo_postal, :telefono])
+  end
 end
